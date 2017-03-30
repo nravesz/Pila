ccb79657758c781eff73e1a4ec2d8033110f9677
xNicky <nraveszani@gmail.com>
2017-03-30 18:47:24 -0300

    Update pruebas_alumno.c

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -31,7 +31,7 @@ void pruebas_pila_alumno(){
     /* Inicio de pruebas */
     print_test("Crear pila con tamanio 10", pila != NULL);
     print_test("Preguntar si la pila esta vacia", pila_esta_vacia(pila) == 1);
-    print_test("Ver el tope de una pila vacia", pila_ver_tope(pila) == NULL);
+    print_test("El tope de una pila vacia es NULL", pila_ver_tope(pila) == NULL);
     
     /* Pruebo que guardar algo en una pila de capacidad 10*/
     print_test("Guardar int", pila_apilar(pila,a));
@@ -58,4 +58,40 @@ void pruebas_pila_alumno(){
 	/* Pruebo agregar un elemento mas para ver si cambia la capacidad */
 	print_test("Guardar char", pila_apilar(pila,k));
     mostrar_capacidad_cantidad(pila);
- }
+    
+    /* Pruebo ver el primer elemento y verifico que no lo desapile.*/
+    print_test("Ver primer elemento de la pila",pila_ver_tope(pila));
+    mostrar_capacidad_cantidad(pila);
+	
+	/* Pruebo desapilar un elemento de una pila vacia */
+	print_test("Desapilar un elemento",pila_desapilar(pila) == k);
+	mostrar_capacidad_cantidad(pila);
+	print_test("Desapilar un elemento",pila_desapilar(pila) == j);
+	mostrar_capacidad_cantidad(pila);
+	print_test("Desapilar un elemento",pila_desapilar(pila) == i);
+	mostrar_capacidad_cantidad(pila);
+	print_test("Desapilar un elemento",pila_desapilar(pila) == h);
+	mostrar_capacidad_cantidad(pila);
+	print_test("Desapilar un elemento",pila_desapilar(pila) == g);
+	mostrar_capacidad_cantidad(pila);
+	print_test("Desapilar un elemento",pila_desapilar(pila) == f);
+	mostrar_capacidad_cantidad(pila);
+	print_test("Desapilar un elemento",pila_desapilar(pila) == e);
+	mostrar_capacidad_cantidad(pila);
+	print_test("Desapilar un elemento",pila_desapilar(pila) == d);
+	mostrar_capacidad_cantidad(pila);
+	print_test("Desapilar un elemento",pila_desapilar(pila) == c);
+	mostrar_capacidad_cantidad(pila);
+	print_test("Desapilar un elemento",pila_desapilar(pila) == b);
+	mostrar_capacidad_cantidad(pila);
+	print_test("Desapilar un elemento",pila_desapilar(pila) == a);
+	mostrar_capacidad_cantidad(pila);
+
+	/* Pruebo que no se pueda desapilar una pila vacia*/
+	print_test("Desapilar un elemento de una pila vacia da false",pila_desapilar(pila) == 0);
+	mostrar_capacidad_cantidad(pila);
+	
+	/* Pruebo destruir la pila*/
+	pila_destruir(pila);
+	print_test("La pila fue destruida", true);
+}
